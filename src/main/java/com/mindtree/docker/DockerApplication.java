241f17c2af189d820281d5f5734971fd75de905d
Mithraa Shree <mithraa1997@gmail.com>
2020-11-13 19:54:29 +0530

    Update DockerApplication.java

--- src/main/java/com/mindtree/docker/DockerApplication.java
+++ src/main/java/com/mindtree/docker/DockerApplication.java
@@ -2,6 +2,7 @@ package com.mindtree.docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class DockerApplication extends SpringBootServletInitializer{
